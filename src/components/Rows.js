062a3820ff7e5e7d445bbc9e4d964b93b9273ed2
RahulRamchandran978 <rahulramchandran978@gmail.com>
2023-11-18 12:00:25 +0530

    Rows

---
+++ src/components/Rows.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Rows = ({title,fetchURL}) => {
+    const [movies,setMovies] = useState([])
+
+    useEffect(()=>{
+        axios.get(fetchURL).then((response)=>{
+            setMovies(response.data.results)
+        })
+    },[fetchURL])
+    console.log(movies)
+
+  return (
+    <>
+    <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+    <div className='relative flex items-flex'>
+        <div id={'slider'}>
+            {movies.map((item,id)=>(
+                <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                    <img src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt={item.title} />
+
+                </div>
+            ))}
+
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Rows
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Main from '../components/Main'
-
+import Rows from '../components/Rows'
+import requests from '../Requests'
 const Home = () => {
   return (
-    <div>
+    <>
       <Main/>
-    </div>
+      <Rows title='UpComing' fetchURL={requests.requestUpcoming}/>
+      <Rows title='Popular' fetchURL={requests.requestPoplular}/>
+      <Rows title='Trending' fetchURL={requests.requestTrending}/>
+      <Rows title='TopRated' fetchURL={requests.requestToprated}/>
+      <Rows title='Horror' fetchURL={requests.requestHorror}/>
+
+      
+    </>
   )
 }
 
