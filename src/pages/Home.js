86e9890d7d1b482586425fba90f0117d8d3d2502
RahulRamchandran978 <rahulramchandran978@gmail.com>
2023-11-22 17:33:05 +0530

    UI-Finsih

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
   return (
     <>
       <Main/>
-      <Rows title='UpComing' fetchURL={requests.requestUpcoming}/>
-      <Rows title='Popular' fetchURL={requests.requestPoplular}/>
-      <Rows title='Trending' fetchURL={requests.requestTrending}/>
-      <Rows title='TopRated' fetchURL={requests.requestToprated}/>
-      <Rows title='Horror' fetchURL={requests.requestHorror}/>
+      <Rows rowID='1' title='Popular' fetchURL={requests.requestPoplular}/>
+      <Rows rowID='2' title='UpComing' fetchURL={requests.requestUpcoming}/>
+      <Rows rowID='3' title='Trending' fetchURL={requests.requestTrending}/>
+      <Rows rowID='4' title='TopRated' fetchURL={requests.requestToprated}/>
+      <Rows rowID='5' title='Horror' fetchURL={requests.requestHorror}/>
 
       
     </>
